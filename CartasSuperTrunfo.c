65c5f6a7e6e69ee77651e82fc23ef3a2cebfcbbf
kauezin0 <kauefmfreitas@gmail.com>
2025-02-21 15:03:51 +0000

    Comparação das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,7 +11,7 @@ int main(){
 
 //Mensagem de instrução para o usuário
     
-    printf("\nBem vindo ao Super Trunfo de Paises!\n\n");
+    printf("\n***Bem vindo ao Super Trunfo de Paises!***\n\n");
     printf("Faca o registro da sua primeira carta:\n\n");
 
 //Registro da primeira carta
@@ -49,14 +49,14 @@ int main(){
 
 //Mensagem de instrução para o usuário
     
-    printf("Carta 1 registrada!!\n\nAgora vamos registrar a carta 2\n\n");
+    printf("\nCarta 1 registrada com sucesso!\n\nAgora vamos registrar a carta 2\n\n");
 
 //Declaração das variáveis da carta 2
     
     char estado2;
     char codigo_da_carta2[50], nome_da_cidade2[50];
     int populacao2, qnt_ponto_turistico2;
-    float area2, pib2, densidade_pop2, pib_per_cap2;
+    float area2, pib2, densidade_pop2, pib_per_cap2, super2;
 
 
 
@@ -89,19 +89,25 @@ int main(){
 
     pib_per_cap2 = (float) pib2/populacao2;
 
-// Cálculo do super poder
+// Cálculo do super poder da carta 2
 
-    super = (float) populacao + qnt_ponto_turistico + area + pib + densidade_pop + pib_per_cap;
+    super2 = (float) populacao + qnt_ponto_turistico + area + pib + densidade_pop + pib_per_cap;
   
 //Exibição dos dados fornecidos pelo usuário
    
-    printf("\nCarta 2 registrada com sucesso!\nEssas sao suas cartas!\nCarta1\n\n");
+    printf("\nCarta 2 registrada com sucesso!\n\n\nEssas sao suas cartas!\n\nCarta1 (1)\n\n---------------------\n");
 
     printf(" Estado: %c\n Codigo da carta: %s\n Nome da cidade: %s\n Populacao: %d\n Area: %.2fkm\n PIB: %.2f\n Numeros de pontos turisticos: %d\n Densidade Populacional: %.2f\n PIB Per Capita: %.2f\n Super Poder: %.3f\n", estado, codigo_da_carta, nome_da_cidade, populacao, area, pib, qnt_ponto_turistico, densidade_pop, pib_per_cap, super);
     
-    printf("Carta 2\n\n");
+    printf("---------------------\n\nCarta 2 (0)\n\n---------------------\n");
     
-    printf(" Estado: %c\n Codigo da carta: %s\n Nome da cidade: %s\n Populacao: %d\n Area: %.2fkm\n PIB: %.2f\n Numeros de pontos turisticos: %d\n Densidade Populacional: %.2f\n PIB Per Capita: %.2f\n Super Poder: %.3f\n", estado2, codigo_da_carta2, nome_da_cidade2, populacao2, area2, pib2, qnt_ponto_turistico2, densidade_pop2, pib_per_cap2, super);
+    printf(" Estado: %c\n Codigo da carta: %s\n Nome da cidade: %s\n Populacao: %d\n Area: %.2fkm\n PIB: %.2f\n Numeros de pontos turisticos: %d\n Densidade Populacional: %.2f\n PIB Per Capita: %.2f\n Super Poder: %.3f\n---------------------", estado2, codigo_da_carta2, nome_da_cidade2, populacao2, area2, pib2, qnt_ponto_turistico2, densidade_pop2, pib_per_cap2, super);
+
+//Resultado da comparação das cartas
+//Densidade pipulacional com o operador NOT
+
+    printf("\nResultado\n\n");
+    printf(" Populacao: %d\n Area: %d\n PIB: %d\n Pontos Turisticos: %d\n Densidade Populacional: %d\n PIB per Capita: %d\n Super Poder: %d\n\n", populacao>populacao2, area>area2, pib>pib2, qnt_ponto_turistico>qnt_ponto_turistico2, ! densidade_pop>densidade_pop2, pib_per_cap>pib_per_cap2, super>super2);
 
     return 0;
 }
